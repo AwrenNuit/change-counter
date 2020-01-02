1b2017b2be5d967420401f09ba39f48effe86a9f
AwrenNuit <a.rennaker@yahoo.com>
2020-01-02 13:34:56 -0600

    updated database

--- database.sql
+++ database.sql
@@ -4,15 +4,14 @@ CREATE TABLE "change" (
 	"id" serial primary key,
 	"path" varchar(255) not null,
 	"name" varchar(255) not null,
-	"class" varchar(255),
-	"quantity" integer DEFAULT 0
+	"class" varchar(255)
 );
 
 INSERT INTO "change" ("path", "name", "class") VALUES('images/penny.jpg', 'penny', 'coin');
 INSERT INTO "change" ("path", "name", "class") VALUES('images/nickel.jpg', 'nickel', 'coin');
 INSERT INTO "change" ("path", "name", "class") VALUES('images/dime.jpg', 'dime', 'coin');
 INSERT INTO "change" ("path", "name", "class") VALUES('images/quarter.jpg', 'quarter', 'coin');
-INSERT INTO "change" ("path", "name", "class") VALUES('images/one.jpg', 'one-dollar', 'bill');
-INSERT INTO "change" ("path", "name", "class") VALUES('images/five.jpg', 'five-dollar', 'bill');
-INSERT INTO "change" ("path", "name", "class") VALUES('images/ten.jpg', 'ten-dollar', 'bill');
-INSERT INTO "change" ("path", "name", "class") VALUES('images/twenty.jpg', 'twenty-dollar', 'bill');
+INSERT INTO "change" ("path", "name", "class") VALUES('images/one.jpg', 'oneDollar', 'bill');
+INSERT INTO "change" ("path", "name", "class") VALUES('images/five.jpg', 'fiveDollar', 'bill');
+INSERT INTO "change" ("path", "name", "class") VALUES('images/ten.jpg', 'tenDollar', 'bill');
+INSERT INTO "change" ("path", "name", "class") VALUES('images/twenty.jpg', 'twentyDollar', 'bill');
